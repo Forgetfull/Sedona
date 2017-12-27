e6e15f72ce884f93976640bd21a9d70e9701a6d5
forgetful <oksana14888@.com>
2017-12-27 18:34:35 +0500

    😀 полностью сделал адаптивный хедер

--- script.js
+++ script.js
@@ -2,6 +2,10 @@
 //$(document).ready(function(){
 //alert('Ваша версия jQuery ' + jQuery.fn.jquery);
 //});
-
-$(".cont").css("background-color","red");
+//
+//$(".cont").css("background-color","red");
 //$("p").css("background-color","red");
+
+//function funk(){
+//    $(".nav-menu").css("display","none");
+//}
